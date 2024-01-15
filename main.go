176c9b9d04768787eef67f073bb180395358ea65
leilafarsani <108967493+leilafarsani@users.noreply.github.com>
2024-01-15 21:48:33 +0000

    constants

--- main.go
+++ main.go
@@ -39,6 +39,10 @@ func main(){
 	typeConversion()
 	v := 42 
 	fmt.Printf("v is of type %T\n", v)
+	constant() 
+	fmt.Println(needInt(Small))
+	fmt.Println(needFloat(Small))
+	fmt.Println(needFloat(Big))
 }
 
 
@@ -86,4 +90,26 @@ func typeConversion() {
 	var f float64 = math.Sqrt(float64(x*x + y*y))
 	var z uint = uint(f)
 	fmt.Println(x, y, z)
+}
+const Pi = 3.14
+func constant() {
+	const World = "世界"
+	fmt.Println("Hello", World)
+	fmt.Println("Happy", Pi, "Day")
+
+	const Truth = true
+	fmt.Println("Go rules?", Truth)
+}
+
+const (
+	// Create a huge number by shifting a 1 bit left 100 places.
+	// In other words, the binary number that is 1 followed by 100 zeroes.
+	Big = 1 << 100
+	// Shift it right again 99 places, so we end up with 1<<1, or 2.
+	Small = Big >> 99
+)
+
+func needInt(x int) int { return x*10 + 1 }
+func needFloat(x float64) float64 {
+	return x * 0.1
 }
