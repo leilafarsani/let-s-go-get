b12a9f8832429e37dcae55fc3a0c1187f1ab64e5
leilafarsani <108967493+leilafarsani@users.noreply.github.com>
2024-01-15 20:36:15 +0000

    return

--- main.go
+++ main.go
@@ -20,6 +20,9 @@ func main(){
 	fmt.Printf("Now you have %g problems.\n", math.Sqrt(17))
 	fmt.Println(math.Pi)
 	fmt.Println(add(42, 13))
+	a, b := swap("hello", "world")
+	fmt.Println(a, b)
+	fmt.Println(split(17))
 
 }
 
@@ -35,5 +38,14 @@ func add(x int, y int) int {
 
 func multiply(x, y int) int {
 	return x * y
+}
+func swap(x, y string) (string, string) {
+	return y, x
+}
+
+func split(sum int) (x, y int) {
+	x = sum * 4 / 9
+	y = sum - x
+	return
 }
 
