d5bad75802042351502dc1ee1635dbbc9fab2bdc
leilafarsani <108967493+leilafarsani@users.noreply.github.com>
2024-01-18 15:47:44 +0000

    if statement

--- main.go
+++ main.go
@@ -46,6 +46,15 @@ func main(){
 	getSum()
 	simpleSum()
 	forLikeWhileGetSum()
+	fmt.Println(sqrt(2), sqrt(-4))
+	fmt.Println(
+		pow(3, 2, 10),
+		pow(3, 3, 20),
+	)
+	fmt.Println(
+		power(3, 1, 10),
+		power(3, 5, 20),
+	)
 }
 
 
@@ -141,3 +150,29 @@ func forLikeWhileGetSum() {
 	}
 	fmt.Println(sum)
 }
+
+func sqrt(x float64) string {
+	if x < 0 {
+		return sqrt(-x) + "i"
+	}
+	return fmt.Sprint(math.Sqrt(x))
+}
+
+func pow(x, n, lim float64) float64 {
+	if v := math.Pow(x, n); v < lim {
+		return v
+	}
+	return lim
+}
+
+func power(x, n, lim float64) float64 {
+	if v := math.Pow(x, n); v < lim {
+		return v
+	} else {
+		fmt.Printf("%g >= %g\n", v, lim)
+	}
+	// can't use v here, though
+	return lim
+}
+
+//(Both calls to pow return their results before the call to fmt.Println in main begins.)
