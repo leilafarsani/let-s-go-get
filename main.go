e51390bdaa668a10df4fd86e1482251606afd118
leilafarsani <108967493+leilafarsani@users.noreply.github.com>
2024-01-20 14:03:00 +0000

    switch practice

--- main.go
+++ main.go
@@ -6,6 +6,7 @@ import (
 	"math/rand"
 	"math"
 	"math/cmplx"
+	"runtime"
 )
 
 var c, python, java bool
@@ -55,6 +56,9 @@ func main(){
 		power(3, 1, 10),
 		power(3, 5, 20),
 	)
+	switchPractice()
+	switchPractice2()
+	switchPractice3()
 }
 
 
@@ -176,3 +180,44 @@ func power(x, n, lim float64) float64 {
 }
 
 //(Both calls to pow return their results before the call to fmt.Println in main begins.)
+
+func switchPractice() {
+	fmt.Print("Go runs on ")
+	switch os := runtime.GOOS; os {
+	case "darwin":
+		fmt.Println("OS X.")
+	case "linux":
+		fmt.Println("Linux.")
+	default:
+		// freebsd, openbsd,
+		// plan9, windows...
+		fmt.Printf("%s.\n", os)
+	}
+}
+
+func switchPractice2() {
+	fmt.Println("When's Saturday?")
+	today := time.Now().Weekday()
+	switch time.Saturday {
+	case today + 0:
+		fmt.Println("Today.")
+	case today + 1:
+		fmt.Println("Tomorrow.")
+	case today + 2:
+		fmt.Println("In two days.")
+	default:
+		fmt.Println("Too far away.")
+	}
+}
+
+func switchPractice3() {
+	t := time.Now()
+	switch {
+	case t.Hour() < 12:
+		fmt.Println("Good morning!")
+	case t.Hour() < 17:
+		fmt.Println("Good afternoon.")
+	default:
+		fmt.Println("Good evening.")
+	}
+}
