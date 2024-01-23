aaee98b59648307424edfee4d4ec48a8fa42e792
leilafarsani <108967493+leilafarsani@users.noreply.github.com>
2024-01-23 13:44:51 +0000

    pointers To Structs Practice fixed

--- main.go
+++ main.go
@@ -64,7 +64,7 @@ func main(){
 	pointerPractice()
 	fmt.Println(Vertex{1, 2})
 	structPractice()
-	PointerToStructPractice()
+	pointersToStructsPractice()
 }
 
 
@@ -271,13 +271,12 @@ type Vertex struct {
 	X, Y int
 }
 
-var (
-	v1 = Vertex{1, 2}  // has type Vertex
-	v2 = Vertex{X: 1}  // Y:0 is implicit
-	v3 = Vertex{}      // X:0 and Y:0
-	n  = &Vertex{1, 2} // has type *Vertex
-)
-
-func PointerToStructPractice() {
-	fmt.Println(v1, n, v2, v3)
+func pointersToStructsPractice() {
+	v := Vertex{1, 2}
+	p := &v
+	p.X = 1e9
+	fmt.Println(v)
 }
+
+
+
