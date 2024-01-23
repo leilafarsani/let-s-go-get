21b64a7880897f629a77e8e32ee811ee9a1e1e13
leilafarsani <108967493+leilafarsani@users.noreply.github.com>
2024-01-23 13:34:44 +0000

    Pointer To Struct Practice

--- main.go
+++ main.go
@@ -64,6 +64,7 @@ func main(){
 	pointerPractice()
 	fmt.Println(Vertex{1, 2})
 	structPractice()
+	PointerToStructPractice()
 }
 
 
@@ -255,13 +256,28 @@ func pointerPractice() {
 	fmt.Println(j) // see the new value of j
 }
 // Struct
-type Vertex struct {
-	X int
-	Y int
-}
+// type Vertex struct {
+// 	X int
+// 	Y int
+// }
 
 func structPractice() {
 	v := Vertex{1, 2}
 	v.X = 4
 	fmt.Println(v.X)
+}
+
+type Vertex struct {
+	X, Y int
+}
+
+var (
+	v1 = Vertex{1, 2}  // has type Vertex
+	v2 = Vertex{X: 1}  // Y:0 is implicit
+	v3 = Vertex{}      // X:0 and Y:0
+	n  = &Vertex{1, 2} // has type *Vertex
+)
+
+func PointerToStructPractice() {
+	fmt.Println(v1, n, v2, v3)
 }
