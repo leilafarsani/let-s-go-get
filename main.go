70ac076a706f77556d5139c73642a737b51da7f3
leilafarsani <108967493+leilafarsani@users.noreply.github.com>
2024-01-23 13:46:35 +0000

    struct Literals Practice

--- main.go
+++ main.go
@@ -65,6 +65,7 @@ func main(){
 	fmt.Println(Vertex{1, 2})
 	structPractice()
 	pointersToStructsPractice()
+	structLiteralsPractice()
 }
 
 
@@ -276,6 +277,16 @@ func pointersToStructsPractice() {
 	p := &v
 	p.X = 1e9
 	fmt.Println(v)
+}
+var (
+	v1 = Vertex{1, 2}  // has type Vertex
+	v2 = Vertex{X: 1}  // Y:0 is implicit
+	v3 = Vertex{}      // X:0 and Y:0
+	n  = &Vertex{1, 2} // has type *Vertex
+)
+
+func structLiteralsPractice() {
+	fmt.Println(v1, n, v2, v3)
 }
 
 
